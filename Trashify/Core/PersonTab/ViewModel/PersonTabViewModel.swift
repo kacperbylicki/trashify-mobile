9de394813dd41f5a35e0bd0e77dc7b9788b14b05
Marek Gerszendorf <>
2023-10-09 20:45:05 +0200

    refactor: clean up code

--- Trashify/Core/PersonTab/ViewModel/PersonTabViewModel.swift
+++ Trashify/Core/PersonTab/ViewModel/PersonTabViewModel.swift
@@ -24,7 +24,6 @@ class PersonTabViewModel: ObservableObject {
     }
     
     private func validateEmail() -> Bool {
-        print("email")
         let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegex)
         return emailTest.evaluate(with: newEmail)
